10a2cefb63cf6bd114cfd11e324cae0286ee34f8
unknown <catalincebuc99@gmail.com>
2022-05-12 02:49:10 +0300

    deploy15

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import MainPage from "./MainPage";
 import Login from "./Login";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import {useEffect} from 'react'
-import {gapi} from 'react-google-login'
+
 
 const clientID = "283208679130-4mbpb2u39fkh4oi0l4l4raf0mpd1n30v.apps.googleusercontent.com";
 

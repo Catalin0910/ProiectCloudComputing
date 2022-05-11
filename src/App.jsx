8f05c3b8ed33b6f7019425b3c2bfcfbd4c47c6ea
unknown <catalincebuc99@gmail.com>
2022-05-12 01:57:50 +0300

    deploy7

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,6 @@ const clientID = "283208679130-4mbpb2u39fkh4oi0l4l4raf0mpd1n30v.apps.googleuserc
 
 function App() {
 
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: clientID,
-        scope: ""
-      })
-    }
-
-    gapi.load('client:auth2', start)
-  })
-
-
   return (
     <div>
       <BrowserRouter>

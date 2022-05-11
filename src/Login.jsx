f9fb95e9181a80993edb68f1626b9de62eb3ba1a
unknown <catalincebuc99@gmail.com>
2022-05-12 01:55:39 +0300

    deploy6

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import GoogleLogin from "react-google-login";
 import { useHistory } from "react-router-dom";
 
+const clientID = "283208679130-4mbpb2u39fkh4oi0l4l4raf0mpd1n30v.apps.googleusercontent.com";
+
 const Login = () => {
   const responseGoogle = (response) => {
     console.log(response);
@@ -9,17 +11,18 @@ const Login = () => {
   };
 
   const history = useHistory();
-  const handleLoginData = (event) => {
-    history.push("/ProiectCloudComputing/main");
-  };
+  const handleLoginData = (res) => {
+    console.log("LOGIN SUCCES! Current user: ", res.profileObj)  
+   };
 
   return (
     <div>
       <GoogleLogin
-        clientId="283208679130-4mbpb2u39fkh4oi0l4l4raf0mpd1n30v.apps.googleusercontent.com"
+        clientId={clientID}
         buttonText="Login"
         onSuccess={handleLoginData}
         onFailure={responseGoogle}
+        isSignedIn={true}
         href="/ProiectCloudComputing/main"
       />
     </div>

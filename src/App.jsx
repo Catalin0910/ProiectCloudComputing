1084ba94d60576ee7ab83e704b6eddf804543757
unknown <catalincebuc99@gmail.com>
2022-05-12 03:49:10 +0300

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import MainPage from "./MainPage";
 import Login from "./Login";
-import { Route, Switch} from "react-router-dom";
+import { Route, Switch, HashRouter} from "react-router-dom";
 
 
 function App() {
 
   return (
     <div>
-        <Switch>
-          <Route exact from="" render={(props) => <Login {...props} />} />
-          <Route path='/main' exact component={MainPage}/>
-        </Switch>
+      <HashRouter>
+       <Switch>
+         <Route path='/' exact component={Login}/>
+         <Route path='/main' exact component={MainPage}/>
+       </Switch>
+     </HashRouter>
     </div>
   );
 }
